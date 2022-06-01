fb672fd2674f7de782e497b18cce7e02723cd12d
Carlos Rincón <carlos.rincon@uniagustiniana.edu.co>
2022-05-31 23:54:16 -0500

    testing avatar embed image

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,19 +4,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
-		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
-		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
+		.setDescription('Visualiza el avatar tuyo o el del usuario que elijas.')
+		.addUserOption(option => option.setName('usuario').setDescription('el avatar del usuario que deseas ver')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
+		const user = interaction.options.getUser('usuario');
 
         const avatarEmbed = new MessageEmbed()
 
         if (!user) {
+            console.log(interaction.user)
             avatarEmbed
             .setColor('GOLD')
             .setTitle(`${interaction.user.username}'s Avatar`)
             .setImage(`${interaction.user.displayAvatarURL({ dynamic: true, format: 'png', size: 600 })}`);
         }else{
+            console.log(user)
             avatarEmbed
              .setColor('GOLD')
              .setTitle(`${user.username}'s Avatar`)
