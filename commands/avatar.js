68a0d447e7d1a0433f2c60c341282bdde3dcad2a
Carlos Rincón <carlos.rincon@uniagustiniana.edu.co>
2022-05-31 23:16:57 -0500

    testing avatar embed

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,12 +10,17 @@ module.exports = {
 		const user = interaction.options.getUser('target');
         console.log(interaction.user.avatarURL())
 
-        const avatarEmbed = new MessageEmbed()
+        const avatarSelfEmbed = new MessageEmbed()
         .setColor('GOLD')
         .setTitle(`${interaction.user.username}'s Avatar`)
         .setImage(`${interaction.user.displayAvatarURL({ dynamic: true })}`)
+        
+        const avatarEmbed = new MessageEmbed()
+        .setColor('GOLD')
+        .setTitle(`${user.username}'s Avatar`)
+        .setImage(`${user.displayAvatarURL({ dynamic: true })}`)
 
 		if (user) return interaction.reply({embeds: [avatarEmbed]});
-		return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
+		return interaction.reply({embeds: [avatarSelfEmbed]});
 	},
 };
